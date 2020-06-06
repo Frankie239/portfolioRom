e79021825355b4687439ebd6486d9a385f04809f
Frankie239 <55329447+Frankie239@users.noreply.github.com>
2020-06-06 14:06:44 -0300

    Proyecrtos ordenados y modificados y dashboard
    Modificaciones en el apartado de proyectos, agregue un dashboard que redirige a los proyectos, modifique el estilo de los proyectos y de el contenedor para hacerlo mas facil visualmente

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function Hamburger() {
     var x = document.getElementById("links");
     if (x.style.display === "block") {
       x.style.display = "none";
-    } else {
+    }
+    
+    else {
       x.style.display = "block";
+      
     }
   } 
+
+
+  
